ba97bfdb417c3ff5a7c19c1e8c97c26bb6538351
DanielChenSapient <52332774+DanielChenSapient@users.noreply.github.com>
2019-07-08 16:54:30 -0500

    Add files via upload

--- IndustryChallengeLambda.py
+++ IndustryChallengeLambda.py
@@ -31,6 +31,149 @@ EXCEPTION_MESSAGE = "Sorry. I cannot help you with that."
 # TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-python-fact/lambda/data
 # =========================================================================================================================================
 
+def request(host, path, api_key, url_params=None):
+    """Given your API_KEY, send a GET request to the API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+#    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+#    print(url)
+    return response.json()
+
+
+def search(api_key, term, location):
+    """Query the Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': 50
+    }
+    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+# This client code can run on Python 2.x or 3.x.  Your imports can be
+# simpler if you only need one of those.
+try:
+    # For Python 3.0 and later
+    from urllib.error import HTTPError
+    from urllib.parse import quote
+    from urllib.parse import urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2 and urllib
+    from urllib2 import HTTPError
+    from urllib import quote
+    from urllib import urlencode
+
+
+# Yelp Fusion no longer uses OAuth as of December 7, 2017.
+# You no longer need to provide Client ID to fetch Data
+# It now uses private keys to authenticate requests (API Key)
+# You can find it on
+# https://www.yelp.com/developers/v3/manage_app
+API_KEY= "bX-0JbMtPZr796pdD-2lVwUgQDcSnJluQsWZb56-qWdTve8A2gC3xVpScikhmvks263_NKA4I4O4oUJNTbPTqTEs_LWl7gEDDph6VB3Fs4q7Ackih0G_IYChgmMWXXYx"
+
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+CATEGORIES_PATH = "/v3/categories/"
+
+# Defaults for our simple example.
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 10
+
+def categories(api_key,term):
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'limit': 50
+    }
+    return request(API_HOST, CATEGORIES_PATH, api_key, url_params = url_params)
+
+def get_business(api_key, business_id):
+    """Query the Business API by a business ID.
+    Args:
+        business_id (str): The ID of the business to query.
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, api_key)        
+
+def query_api(term, location):
+    """Queries the API by the input values from the user.
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    response = search(API_KEY, term, location)
+
+    businesses = response.get('businesses')
+
+    if not businesses:
+        print(u'No businesses for {0} in {1} found.'.format(term, location))
+        return
+
+    business_id = businesses[0]['id']
+
+    print(u'{0} businesses found, querying business info ' \
+        'for the top result "{1}" ...'.format(
+            len(businesses), business_id))
+    response = get_business(API_KEY, business_id)
+
+    print(u'Result for business "{0}" found:'.format(business_id))
+    pprint.pprint(response, indent=2)
+    
+Locations = ["Chicago","New York","LA","Boulder","San Fransico","Austin","Toronto","Atlanta","Miami","Paris","London","Berlin"]
+location_data = {}
+for l in Locations:
+#    print(l)
+    r = search(API_KEY, "",l)
+    businesses = []
+    
+    for b in r['businesses']:
+        temp_dict={}
+        if 'price' in b.keys():
+            temp_dict = {'name': b['name'],'price':b['price'],'rating':b['rating'],"categories":[x['alias'] for x in b['categories']]}
+        else:
+            temp_dict = {'name': b['name'],'rating':b['rating'],"categories":[x['alias'] for x in b['categories']]}
+            
+        businesses.append(temp_dict)
+    location_data[l] = businesses
+
+
+
 data = [
   'John Smith. I see that you went to the aqurium in san diego last week. Might I suggest checking out the Shed Aquarium in downtown chicago?',
   'Becky Budro. Welcome to Chi Town! I know you love art museums! We are about two blocks from the MFA, you should go check it out!'
